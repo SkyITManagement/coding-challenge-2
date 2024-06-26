481737bacb5107a1db3cc8b7d64ca80731876252
chiaosoh <chiao.soh@gmail.com>
2024-06-25 20:02:11 -0400

    added directions in serializers.py

---
+++ django_rest_challenge/challenge/serializers.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import Group, User
+from rest_framework import serializers
+
+# TODO: Add serializers for the User and Group models
+
+# User Serializer - Fields: url, username, email, groups
+
+# Groups Serializer - Fields: url, name
+
